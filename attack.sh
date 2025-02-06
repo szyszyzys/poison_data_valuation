8e50cad72d1801209d955c0e44ce8466b20e8722
Zeyu Song <szyszy0315@gmail.com>
2025-02-05 22:13:07 -0500

    f

--- attack.sh
+++ attack.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+WORK_DIR="/scratch/zzs5287/poison_data_valuation"
+export PYTHONPATH="$WORK_DIR:$PYTHONPATH"
+echo $PYTHONPATH
+
 python attack/privacy_attack/run.py --dataset gaussian
 
 python attack/privacy_attack/run.py --dataset mimic
