93d7ba6a3da75b7dceca665c6bdd1834b9bf2691
Zeyu Song <szyszy0315@gmail.com>
2025-02-06 00:30:44 -0500

    f

--- attack/privacy_attack/run.py
+++ attack/privacy_attack/run.py
@@ -301,7 +301,8 @@ def run_attack_experiment(dataset_type="gaussian", dim=100, num_seller=1000,
             # append_result("score_unknown_ranking_logistic", score_unknown_ranking_logistic)
             # append_result("score_unknown_topk", score_unknown_topk)
 
-    plot_results_utility(result_path, {"errors": selection_errors["DAVED"]}, costs)
+    plot_results_utility(result_path, {"errors": selection_errors["DAVED"], "eval_range": max_eval_range_selection_num},
+                         costs)
 
     # After the loop, save all accumulated results to a single CSV file.
     filename = save_attack_results(results)
