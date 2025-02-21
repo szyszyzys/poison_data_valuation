06f047bdc54a4c5152af21167c16e53bc83fc8ce
Zeyu Song <szyszy0315@gmail.com>
2025-02-21 08:40:04 -0500

    f

--- script/entry/run_exp/backdoor_all.sh
+++ script/entry/run_exp/backdoor_all.sh
@@ -11,5 +11,3 @@ bash script/gradient_script/backdoor.sh --poison_strength "0.1,0.5,1.0" --gradie
 
 bash script/gradient_script/backdoor.sh --poison_strength "0.1,0.5,1.0" --gradient_manipulation_mode "cmd" --n_adversaries "1,2,3,4,5" --local_epoch 2 --local_lr 1e-2 --dataset_name CIFAR
 
-
-rsync -av --exclude='*.pt' zzs5287@E5-cse-cbsjm01.eecscl.psu.edu:/scratch/zzs5287/poison_data_valuation/results/backdoor/FMINIST/backdoor_mode_single_strength_0.1/  /c/Users/zeyu song/Desktop/codes/poison_data_valuation/result
