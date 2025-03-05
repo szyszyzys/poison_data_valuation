8c9d14c57c6934b0d528f0fd05cf4337dc91f8df
Zeyu Song <szyszy0315@gmail.com>
2025-03-05 01:10:54 -0500

    f

--- script/entry/run_exp/attack_backdoor_local_single.sh
+++ script/entry/run_exp/attack_backdoor_local_single.sh
@@ -23,10 +23,10 @@ SYBIL_MODES=("mimic")
 N_ADVS=(0.2 0.3)
 ALPHAS=(0.5)
 data_split_modes=("dirichlet" "label")
-#IS_SYBIL_VALUES=("False" "True")
-IS_SYBIL_VALUES=("True")
-#TIGGER_MODES=("static" "dynamic")
-TIGGER_MODES=("dynamic")
+IS_SYBIL_VALUES=("False" "True")
+#IS_SYBIL_VALUES=("True")
+TIGGER_MODES=("static" "dynamic")
+#TIGGER_MODES=("dynamic")
 # For each combination, we call run_experiment.py with the corresponding args.
 # In practice, you might want to limit how big this grid is, or do partial loops.
 
