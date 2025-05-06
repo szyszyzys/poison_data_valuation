697539b3921e2004e7e90432d7b1d43f78c1264c
songzeyu002 <szyszy0315@gmail.com>
2025-05-06 00:52:16 -0700

    f

--- entry/gradient_market/automate_exp/config_generation.py
+++ entry/gradient_market/automate_exp/config_generation.py
@@ -61,7 +61,7 @@ BASE_CONFIG_TEMPLATE = {
         'buyer_percentage': 0.02,
         'data_split_mode': 'discovery',
         'dm_params': {  # Used if data_split_mode="discovery"
-            'discovery_quality': 0.7,  # Example default
+            'discovery_quality': 0.3,  # Example default
             'buyer_data_mode': 'unbiased',  # Example default ('random' or 'biased')
         },
         'normalize_data': True,
@@ -131,7 +131,6 @@ BASE_CONFIG_TEMPLATE = {
 }
 
 DATASETS = ['AG_NEWS', 'TREC', 'CIFAR', 'FMNIST']
-# DATASETS = ['CIFAR', 'FMNIST']
 AGGREGATIONS = ['fedavg', 'martfl', "skymask", "fltrust"]
 # --- Model Configs per Dataset (Simplified) ---
 # You might need more details (layers, etc.) depending on model structure definition
@@ -214,7 +213,7 @@ def generate_backdoor_attack_configs(output_dir):
     """Vary attack params: adv_rate, aggregation, maybe trigger/target."""
     print("\n--- Generating Attack Configs ---")
     datasets = DATASETS
-    adv_rates = [0.1, 0.2, 0.3, 0.4]
+    adv_rates = [0.3]
 
     aggregations = AGGREGATIONS  # Compare how Sybil affects different aggregators
     target_labels = [0]  # Could vary this too
@@ -261,7 +260,7 @@ def generate_label_flipping_attack_configs(output_dir):
     """Vary attack params: adv_rate, aggregation, maybe trigger/target."""
     print("\n--- Generating Attack Configs ---")
     datasets = DATASETS  # Focus on one dataset for this example
-    adv_rates = [0.1, 0.2, 0.3, 0.4]
+    adv_rates = [0.3]
     aggregations = AGGREGATIONS  # Compare how Sybil affects different aggregators
     target_labels = [0]  # Could vary this too
     flip_modes = ['blended_patch']  # Could vary
@@ -302,7 +301,7 @@ def generate_sybil_configs(output_dir):
     """Focus on varying Sybil parameters."""
     print("\n--- Generating Sybil Attack Configs ---")
     datasets = DATASETS
-    adv_rates = [0.1, 0.2, 0.3, 0.4]  # Fix adversary rate
+    adv_rates = [0.3]  # Fix adversary rate
     aggregations = AGGREGATIONS  # Compare how Sybil affects different aggregators
     amplify_factors = [1.0]  # Vary amplification
     attack_modes = ["None", BACKDOOR, LABEL_FLIP]
@@ -442,7 +441,7 @@ if __name__ == "__main__":
         # Manually set device in template if torch is unavailable
         BASE_CONFIG_TEMPLATE['device'] = 'cpu'
 
-    CONFIG_OUTPUT_DIRECTORY = "./configs_generated"  # Directory to save generated configs
+    CONFIG_OUTPUT_DIRECTORY = "./configs_generated_new"  # Directory to save generated configs
 
     print(f"Generating configuration files in: {CONFIG_OUTPUT_DIRECTORY}")
 
